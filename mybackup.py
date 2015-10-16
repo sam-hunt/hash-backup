4b228c7c188b5099b7defbb0cef10518111ff94f
Sam Hunt <s2112201@ipc.ac.nz>
2015-10-17 00:59:50 +1300

    Logger module added, main command switch structure defined

--- mybackup.py
+++ mybackup.py
@@ -4,13 +4,63 @@ Specifications as per 159.251 Software Engineering Design and Construction Assig
 """
 
 import sys
+import os
+import os.path
+import logger
 
 from mkfilesig import create_file_signature
 
 __author__ = 'Sam Hunt'
 
+
+def help_():
+    pass
+
+
+def init():
+    pass
+
+
+def store():
+    pass
+
+
+def list_():
+    pass
+
+
+def test():
+    pass
+
+
+def get():
+    pass
+
+
+def restore():
+    pass
+
+
 if __name__ == '__main__':
 
-    if sys.argv[1] not in ['init', 'store', 'list', 'test', 'get', 'restore']:
-        raise ValueError("command must be one of 'init', 'store', 'list', 'test', 'get', 'restore'")
-        sys.exit(1)
+    # the command entered by the user
+    command = hash(sys.argv[1].strip().lower())
+
+    # all commands supported by the program
+    commands = {hash('init'): init,
+                hash('store'): store,
+                hash('list'): list_,
+                hash('test'): test,
+                hash('get'): get,
+                hash('restore'): restore,
+                hash('help'): help_}
+
+    # ensure the user has entered a supported command
+    if command not in commands.keys():
+        raise ValueError("command must be one of 'help', 'init', 'store', 'list', 'test', 'get', 'restore'")
+
+    # switch on command
+    commands[command]()
+
+    # return execution to os
+    sys.exit(0)
